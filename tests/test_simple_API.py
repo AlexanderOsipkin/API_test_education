b4ef79f752385aa17131aff37128e1a4aa469465
alexanderosipkin <alexander.osipkin@umbrellait.com>
2024-07-18 13:29:05 +0300

    adding new API test and code refactoring

--- tests/test_simple_API.py
+++ tests/test_simple_API.py
@@ -4,9 +4,21 @@ import requests
 from jsonschema import validate
 
 
-def test():
-    response = requests.post('https://reqres.in/api/users', data={'name':'morpheus', 'job':'leader'})
+def test_json_schema_in_response():
+    response = requests.post('https://reqres.in/api/users', data={'name': 'morpheus', 'job': 'leader'})
     body = response.json()
+
     assert response.status_code == 201
     with open('post_users.json') as file:
         validate(body, schema=json.loads(file.read()))
+
+
+def test_job_name_from_request_returns_in_response():
+    job = "leader"
+    name = "morpheus"
+
+    response = requests.post('https://reqres.in/api/users', data={'name': name, 'job': job})
+    body = response.json()
+
+    assert body['name'] == name
+    assert body['job'] == job
