08126b3cdf5d26abaf792d9ae7ec64e7d1961160
alexanderosipkin <alexander.osipkin@umbrellait.com>
2024-07-18 13:13:14 +0300

    adding simple API test

--- tests/test_simple_API.py
+++ tests/test_simple_API.py
@@ -1,3 +1,12 @@
+import json
 
-def test_api():
-    test assert test
+import requests
+from jsonschema import validate
+
+
+def test():
+    response = requests.post('https://reqres.in/api/users', data={'name':'morpheus', 'job':'leader'})
+    body = response.json()
+    assert response.status_code == 201
+    with open('post_users.json') as file:
+        validate(body, schema=json.loads(file.read()))
