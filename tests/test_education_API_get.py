8bd2b3b05a4891f2011470451d32f61c009a2622
alexanderosipkin <aleksandrosipkin@yandex.ru>
2024-07-25 09:12:21 +0300

    refactoring get test

--- tests/test_education_API_get.py
+++ tests/test_education_API_get.py
@@ -2,11 +2,7 @@ import requests
 
 
 def test_get_pet_by_status():
-    url = 'https://petstore.swagger.io/v2/pet/findByStatus'
-    status = {"status": "available"}
-    result = requests.get(
-        url,
-        status
-    )
-    assert result.status_code == 200
+    response = requests.get('https://petstore.swagger.io/v2/pet/findByStatus', data={"status": "available"})
+
+    assert response.status_code == 200
 
