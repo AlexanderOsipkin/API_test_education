f269b4b3f46c5e7756ea148d1f724da2e8616efc
alexanderosipkin <aleksandrosipkin@yandex.ru>
2024-07-25 10:29:23 +0300

    refactoring create user request

--- tests/test_education_API_post.py
+++ tests/test_education_API_post.py
@@ -6,15 +6,15 @@ from jsonschema import validate
 
 def test_create_user():
     request_body_data = {
-  "id": 0,
-  "username": "string",
-  "firstName": "string",
-  "lastName": "string",
-  "email": "string",
-  "password": "string",
-  "phone": "string",
-  "userStatus": 0
-}
+        "id": 157893627,
+        "username": "testuser157",
+        "firstName": "Ivan",
+        "lastName": "Ivanov",
+        "email": "test@testmail.com",
+        "password": "test157",
+        "phone": "+79273774254",
+        "userStatus": 0
+    }
     response = requests.post('https://petstore.swagger.io/v2/user', json=request_body_data)
     body = response.json()
 
