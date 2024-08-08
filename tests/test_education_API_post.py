5ff920d7626b1228f11306b1d093a7a3fb42ec63
alexanderosipkin <aleksandrosipkin@yandex.ru>
2024-08-08 10:57:32 +0300

    minor fix

--- tests/test_education_API_post.py
+++ tests/test_education_API_post.py
@@ -7,12 +7,12 @@ from jsonschema import validate
 def test_create_user():
     request_body_data = {
         "id": 157893627,
-        "username": "testuser157",
+        "username": "testuser1579",
         "firstName": "Ivan",
         "lastName": "Ivanov",
         "email": "test@testmail.com",
-        "password": "test157",
-        "phone": "+79273774254",
+        "password": "test1579",
+        "phone": "+79999999999",
         "userStatus": 0
     }
     response = requests.post('https://petstore.swagger.io/v2/user', json=request_body_data)
