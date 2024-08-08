4079b585ef1d19311dd38f4ff1ad3752ede204d4
alexanderosipkin <aleksandrosipkin@yandex.ru>
2024-08-08 10:58:47 +0300

    minor fix

--- tests/test_education_API_get.py
+++ tests/test_education_API_get.py
@@ -2,7 +2,8 @@ import requests
 
 
 def test_get_pet_by_status():
-    response = requests.get('https://petstore.swagger.io/v2/pet/findByStatus', data={"status": "available"})
+    response = requests.get('https://petstore.swagger.io/v2/pet/findByStatus',
+                            data={"status": "available"})
 
     assert response.status_code == 200
 
