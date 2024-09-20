a441266bc88306405e3adf175c1aad7055e02ed5
alexanderosipkin <aleksandrosipkin@yandex.ru>
2024-09-20 14:01:22 +0300

    code refactoring

--- tests/test_update_user.py
+++ tests/test_update_user.py
@@ -5,21 +5,21 @@ from jsonschema import validate
 
 
 def test_update_user():
-    def test_create_user():
-        request_body_data = {
-            "id": 157893627,
-            "username": "testuser157900",
-            "firstName": "Ivan1",
-            "lastName": "Ivanov",
-            "email": "test@testmail.com",
-            "password": "test1579",
-            "phone": "+79999999999",
-            "userStatus": 0
-        }
+    username = "testuser157900"
+    request_body_data = {
+        "id": 157893627,
+        "username": "testuser157900",
+        "firstName": "Ivan12",
+        "lastName": "Ivanov",
+        "email": "test@testmail.com",
+        "password": "test1579",
+        "phone": "+79999999999",
+        "userStatus": 0
+    }
 
-        response = requests.put('https://petstore.swagger.io/v2/user/testuser157900', json=request_body_data)
-        body = response.json()
+    response = requests.put(f'https://petstore.swagger.io/v2/user/{username}', json=request_body_data)
+    body = response.json()
 
-        assert response.status_code == 200
-        with open('education_schema.json') as file:
-            validate(body, schema=json.loads(file.read()))
+    assert response.status_code == 200
+    with open('education_schema.json') as file:
+        validate(body, schema=json.loads(file.read()))
