6d4455d496cda53b345d5130464dba8dde3a67fb
alexanderosipkin <aleksandrosipkin@yandex.ru>
2024-09-20 14:59:58 +0300

    code refactoring

--- tests/test_delete_user.py
+++ tests/test_delete_user.py
@@ -2,6 +2,7 @@ import requests
 
 
 def test_delete_user():
-    response = requests.delete('https://petstore.swagger.io/v2/user/testuser15790033')
+    username = "testuser15790033"
+    response = requests.delete(f'https://petstore.swagger.io/v2/user/{username}')
 
     assert response.status_code == 200
